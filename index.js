12aaf1b2b1b248fa19aa59977d2c2e3239832f73
AnjanaPathirana99 <80908562+AnjanaPathirana99@users.noreply.github.com>
2022-07-02 17:37:01 +0530

    Input validation using Joi init

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const Joi = require('joi')
 const app = express();
 
 app.use(express.json())
@@ -24,6 +25,33 @@ app.get('/api/genres/:id', (req, res) => {
     if (!genre) res.status(404).send('Genre with the given ID was not found');
     res.send(genre);
 })
+
+//Creating a new genre
+
+app.post('/api/genres', (req, res) => {
+    const { error } = validateGenre(req.body);
+    if (error) {
+        res.status(400).send(error.details[0].message);
+        return;
+    }
+
+    const genre = {
+        id: genres.length + 1,
+        name: req.body.name
+    };
+    genres.push(genre);
+    res.send(genre);
+})
+
+
+
+function validateGenre(genre) {
+    const schema = {
+        name: Joi.string().min(3).required()
+    }
+
+    return Joi.validate(genre, schema)
+}
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`))
