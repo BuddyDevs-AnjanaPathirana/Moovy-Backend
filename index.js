2e3b14b835daacc2f6cb44a2fef61beca39a81b8
AnjanaPathirana99 <80908562+AnjanaPathirana99@users.noreply.github.com>
2022-09-05 20:19:41 +0530

    Fix syntax error

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const Joi = require('joi');
 const app = express();
-const genres = require('/Routes/Genres')
+const genres = require('/Routes/Genres');
 
 app.use(express.json())
 app.use('/api/genres', genres);
