681e93d858f929333646c03a8122e9e2aa039cef
Änjana Pathirana <anjanamadhushan44@gmail.com>
2023-02-07 17:06:03 +0530

    Connect to mongodb database

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
-const express = require('express');
-const Joi = require('joi');
+const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
-const genres = require('/Routes/Genres');
+const genres = require("./Routes/Genres");
 
-app.use(express.json())
-app.use('/api/genres', genres);
+mongoose
+  .connect("mongodb://127.0.0.1/moovy")
+  .then(() => console.log("Connected to mongodb..."))
+  .catch((err) => console.error("Could not connect to mongodb..."));
 
-
+app.use(express.json());
+app.use("/api/genres", genres);
 
 //Define environmental variables
 
