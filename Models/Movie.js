9820cb83118efc35ffb16496adfa192f3310e204
Änjana Pathirana <anjanamadhushan44@gmail.com>
2023-02-17 13:17:33 +0530

    Movie.js model init

---
+++ Models/Movie.js
@@ -0,0 +1,50 @@
+const mongoose = require("mongoose");
+const Joi = require("joi");
+const { genreSchema } = require("./Genre");
+
+//###################################  Movie Schema & Create Movie Model  ###################################
+
+const Movie = mongoose.model(
+  "Movie",
+  new mongoose.Schema({
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 5,
+      maxlength: 255,
+    },
+    genre: {
+      type: genreSchema,
+      required: true,
+    },
+    numberInStock: {
+      type: Number,
+      required: true,
+      min: 0,
+      max: 255,
+    },
+    dailyRentalRate: {
+      type: Number,
+      required: true,
+      min: 0,
+      max: 255,
+    },
+  })
+);
+
+//Validate movie using Joi
+
+function validateMovie(movie) {
+  const schema = {
+    title: Joi.string().min(5).max(50).required(),
+    genreId: Joi.string().required(),
+    numberInStock: Joi.number().min(0).required(),
+    dailyRentalRate: Joi.number().min(0).required(),
+  };
+
+  return Joi.validate(movie, schema);
+}
+
+module.exports.Movie = Movie;
+module.exports.validate = validateMovie;
