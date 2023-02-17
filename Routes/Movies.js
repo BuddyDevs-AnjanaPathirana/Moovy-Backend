243e61d20bca06ca27ce76e45d92cabb5fa57123
Änjana Pathirana <anjanamadhushan44@gmail.com>
2023-02-17 13:18:00 +0530

    Movies.js router init

---
+++ Routes/Movies.js
@@ -0,0 +1,83 @@
+const express = require("express");
+const router = express.Router();
+const { validate, Movie } = require("../Models/Movie");
+const { Genre } = require("../Models/Genre");
+
+//###################################  Movies API  ###################################
+
+//Getting all movies
+
+router.get("/", async (req, res) => {
+  const movies = await Movie.find().sort("title");
+  res.send(movies);
+});
+
+//Getting movie with a id
+
+router.get("/:id", async (req, res) => {
+  const movie = await Movie.findById(req.params.id);
+
+  if (!movie)
+    return res.status(404).send("Movie with the given ID was not found");
+  res.send(movie);
+});
+
+//Creating a new movie
+
+router.post("/", async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const genre = await Genre.findById(req.body.genreId);
+  if (!genre) return res.status(400).send("Invalid genre");
+
+  let movie = new Movie({
+    title: req.body.title,
+    genre: {
+      _id: genre._id,
+      name: genre.name,
+    },
+    numberInStock: req.body.numberInStock,
+    dailyRentalRate: req.body.dailyRentalRate,
+  });
+
+  movie = await movie.save();
+  res.send(movie);
+});
+
+//Updating a movie
+
+router.put("/:id", async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const movie = await Movie.findByIdAndUpdate(
+    req.params.id,
+    { title: req.body.title },
+    {
+      genre: {
+        _id: genre._id,
+        name: genre.name,
+      },
+    },
+    { numberInStock: req.body.numberInStock },
+    { dailyRentalRate: req.body.dailyRentalRate }
+  );
+
+  if (!movie)
+    return res.status(404).send("Movie with the given ID was not found");
+  res.send(movie);
+});
+
+//Delete a movie
+
+router.delete("/:id", async (req, res) => {
+  const movie = await Movie.findByIdAndDelete(req.params.id);
+
+  if (!movie)
+    return res.status(404).send("Movie with the given ID was not found");
+
+  res.send(movie);
+});
+
+module.exports = router;
