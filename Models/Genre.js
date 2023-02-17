0ae1b741931b14fa5f4e9fa3bb39111967071351
Änjana Pathirana <anjanamadhushan44@gmail.com>
2023-02-17 13:17:11 +0530

    Update Genre.js model

--- Models/Genre.js
+++ Models/Genre.js
@@ -3,17 +3,16 @@ const Joi = require("joi");
 
 //###################################  Genre Schema & Create Genre Model  ###################################
 
-const Genre = mongoose.model(
-  "Genre",
-  new mongoose.Schema({
-    name: {
-      type: String,
-      required: true,
-      minlength: 5,
-      maxlength: 50,
-    },
-  })
-);
+const genreSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 50,
+  },
+});
+
+const Genre = mongoose.model("Genre", genreSchema);
 
 //Validate genre using Joi
 
@@ -26,4 +25,5 @@ function validateGenre(genre) {
 }
 
 module.exports.Genre = Genre;
+module.exports.genreSchema = genreSchema;
 module.exports.validate = validateGenre;
