8d9ca18aeccf1dcdfbb30b59c9eb92cbcee8ce3c
Änjana Pathirana <anjanamadhushan44@gmail.com>
2023-02-20 16:10:26 +0530

    Add missing validations

--- Models/User.js
+++ Models/User.js
@@ -8,12 +8,14 @@ const User = mongoose.model(
       type: String,
       required: true,
       minlength: 5,
-      maxlength: 255,
+      maxlength: 50,
     },
     email: {
       type: String,
       required: true,
       unique: true,
+      minlength: 5,
+      maxlength: 255,
     },
     password: {
       type: String,
@@ -26,8 +28,9 @@ const User = mongoose.model(
 
 const validateUser = (user) => {
   const schema = {
-    email: Joi.email().required(),
-    password: Joi.string().required().min(5).max(255),
+    name: Joi.string().min(5).max(50).required(),
+    email: Joi.string().min(5).max(255).email().required(),
+    password: Joi.string().min(5).max(255).required(),
   };
   return Joi.validate(user, schema);
 };
