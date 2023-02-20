bcd20e73367bdbaadaed1cc78f17def7d402abd7
Änjana Pathirana <anjanamadhushan44@gmail.com>
2023-02-20 16:10:09 +0530

    Fix syntax errors

--- Routes/Users.js
+++ Routes/Users.js
@@ -10,14 +10,14 @@ router.post("/", async (req, res) => {
   const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
-  const user = await User.find(req.body.email);
-  if (user) return res.status(400).send("User already exists");
+  let user = await User.findOne({ email: req.body.email });
+  if (user) return res.status(400).send("User already registered");
 
-  user = new User(
-    { name: req.body.name },
-    { email: req.body.email },
-    { password: req.body.password }
-  );
+  user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+  });
   await user.save();
   res.send(user);
 });
