58aaf141e8a7a1fcd1861ca89a237861354ba25b
Änjana Pathirana <anjanamadhushan44@gmail.com>
2023-02-20 14:40:33 +0530

    Remove repetitive variables

--- Routes/Rentals.js
+++ Routes/Rentals.js
@@ -28,7 +28,7 @@ router.post("/", async (req, res) => {
   if (movie.numberInStock === 0)
     return res.status(400).send("Movie out of stock");
 
-  let rental = new Rental({
+  const rental = new Rental({
     customer: {
       _id: customer._id,
       name: customer.name,
@@ -41,7 +41,7 @@ router.post("/", async (req, res) => {
     },
   });
 
-  rental = await rental.save();
+  await rental.save();
   movie.numberInStock--;
   movie.save();
 
