0b76779dd8c01054ea9d45ed4ff76d5f725f977e
Änjana Pathirana <anjanamadhushan44@gmail.com>
2023-02-23 10:55:59 +0530

    Auth routes init

--- Routes/auth.js
+++ Routes/auth.js
@@ -1,4 +1,34 @@
 const express = require("express");
 const router = express.Router();
+const { User } = require("../Models/User");
+const Joi = require("joi");
+const bcrypt = require("bcrypt");
+
+//###################################  Auth API  ###################################
+
+//Login a user
+
+router.post("/", async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  let user = await User.findOne({ email: req.body.email });
+  if (!user) return res.status(400).send("Invalid email or password");
+
+  const validPassword = await bcrypt.compare(req.body.password, user.password);
+  if (!validPassword) return res.status(400).send("Invalid email or password");
+
+  console.log("User logged in...");
+
+  res.send(true);
+});
+
+const validate = (req) => {
+  const schema = {
+    email: Joi.string().min(5).max(255).email().required(),
+    password: Joi.string().min(5).max(255).required(),
+  };
+  return Joi.validate(req, schema);
+};
 
 module.exports = router;
