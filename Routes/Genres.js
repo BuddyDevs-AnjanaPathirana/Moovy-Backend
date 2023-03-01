cfecdd0e01ea0b765b5868c63d51d80996d3d62b
Änjana Pathirana <anjanamadhushan44@gmail.com>
2023-03-01 16:19:19 +0530

    Add authorization to protect routes

--- Routes/Genres.js
+++ Routes/Genres.js
@@ -1,3 +1,4 @@
+const auth = require("../Middleware/auth");
 const { Genre, validate } = require("../Models/Genre");
 const express = require("express");
 const router = express.Router();
@@ -22,7 +23,7 @@ router.get("/:id", async (req, res) => {
 
 //Creating a new genre
 
-router.post("/", async (req, res) => {
+router.post("/", auth, async (req, res) => {
   const { error } = validate(req.body);
   if (error) {
     return res.status(400).send(error.details[0].message);
@@ -36,7 +37,7 @@ router.post("/", async (req, res) => {
 
 //Updating a genre
 
-router.put("/:id", async (req, res) => {
+router.put("/:id", auth, async (req, res) => {
   const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
@@ -53,7 +54,7 @@ router.put("/:id", async (req, res) => {
 
 //Delete a genre
 
-router.delete("/:id", async (req, res) => {
+router.delete("/:id", auth, async (req, res) => {
   const genre = await Genre.findByIdAndRemove(req.params.id);
 
   if (!genre) res.status(404).send("Genre with the given ID was not found");
