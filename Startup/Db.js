f3a93969c9fce4c5c7487ddb659c09597e8fce3b
Änjana Pathirana <anjanamadhushan44@gmail.com>
2023-03-10 16:18:32 +0530

    Db connection string stored in .env

--- Startup/Db.js
+++ Startup/Db.js
@@ -1,8 +1,11 @@
+require("dotenv").config();
 const winston = require("winston");
 const mongoose = require("mongoose");
 
 module.exports = () => {
+  mongoose.set("strictQuery", false);
+  const db = process.env.DB_TEST;
   mongoose
-    .connect("mongodb://127.0.0.1/moovy")
-    .then(() => winston.info("Connected to mongodb..."));
+    .connect(db)
+    .then(() => console.log(`Connected to ${process.env.DB_TEST}...`));
 };
